677c0e20c4e9c56136594285ec46be7beb12144a
张强 <email2zq@qq.com>
2022-11-25 17:41:08 +0800

    可以去除python-gi的支持了

--- make.py
+++ make.py
@@ -40,9 +40,6 @@ SITE_SOURCES = {
     'ubuntu-focal': 'ubuntu focal main amd64',
     'ubuntu-jammy': 'ubuntu jammy main amd64'
 }
-EXTRA_PACKAGES = [
-    ('ubuntu focal universe amd64', ('python-gi',))
-]
 
 CACHE = {}
 print_lock = Semaphore()
@@ -208,13 +205,6 @@ async def main():
         deepin_site.open(False)
         with DeleteOnError(OUTPUT, 'wt') as f:
             deepin_site.dump(set.union(*[x[1] for x in result]), f)
-
-            for site_source, pkg_names in EXTRA_PACKAGES:
-                site = await create_site(site_source)
-                site.open(False)
-                for pkg_name in pkg_names:
-                    site.dump([x[0] for x in site.get_package_entries(pkg_name)], f)
-                site.close()
         deepin_site.close()
 
 
